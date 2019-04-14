f2b64dea2bf7ab2d48863c43617f5ace5d58d58a
Your Name <frinklai@gmail.com>
2019-04-14 23:23:13 +0800

    merge all of the label files that belong to same class to one file

---
+++ merge_label.py
@@ -0,0 +1,31 @@
+import sys
+import os
+import os.path
+# import time
+# time1=time.time()
+
+def MergeTxt(filepath, outfile):
+    k = open(filepath+outfile, 'a+')    # create new file (1/2)
+    k = open(filepath+outfile, 'r+')    # clear file contents (1/2)
+    k.truncate()                        # clear file contents (2/2)
+
+    # create new file's contenet (2/2)
+    for parent, dirnames, filenames in os.walk(filepath):
+        for filepath in filenames:
+            txtPath = os.path.join(parent, filepath) 
+            f = open(txtPath)
+            k.write(f.read())
+    print("Finished create merge file: ", parent + outfile)
+    k.close()
+    
+
+if __name__ == '__main__':
+    parent_path = "Labels/"
+    child_path = ['001/', '002/']
+    outfile  = "result.txt"
+
+    for (i, filepath) in enumerate(child_path):
+        filepath = os.path.join(parent_path, child_path[i]) 
+        MergeTxt(filepath, outfile)
+    # time2 = time.time()
+
