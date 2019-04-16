e98156aa12de79b3830a8c8eb4d7bacd74ce27a2
zheken <dogwei111@gmail.com>
2019-04-16 11:49:27 +0800

    change resize method to ratio, not specify size

--- resize_pic.py
+++ resize_pic.py
@@ -5,14 +5,21 @@ import utils as util
 dir_path_list  = util.label_path
 file_type = '.jpg'
 
+desire_ratio = 6
+
 for (i, path) in enumerate(dir_path_list):
     for name in glob.glob('./Orig/' + dir_path_list[i] + '/*'):  
         orig_img = Image.open(name)
         img_name = os.path.split(name)[-1].split('.')[0]
-        out = orig_img.resize((640, 480))
+
+        new_img_w = int(orig_img.size[0]/desire_ratio)
+        new_img_h = int(orig_img.size[1]/desire_ratio)
+        new_img = orig_img.resize((new_img_w, new_img_h))
+
         if (os.path.exists("./Train/" + dir_path_list[i])==False) :
             os.makedirs("./Train/" + dir_path_list[i])
-        out.save("./Train/" + dir_path_list[i] + img_name + file_type)
+        new_img.save("./Train/" + dir_path_list[i] + img_name + file_type)
+        # print('new img:', [new_img.size[0], new_img.size[1]]   )
     print('['+str(i)+'] ' + "Save path : " +"./Train/" + dir_path_list[i] )
 
 
